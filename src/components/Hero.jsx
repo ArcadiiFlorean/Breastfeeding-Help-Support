27940baf481fe5ea45bfd9a3af2910b80402e5bd
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-07-08 12:04:43 +0100

    Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,14 @@ function Hero() {
       <img
         src="/flowers-bg.jpg"
         alt="Fundal flori"
-        className=" tablet:block absolute bottom-[80px] tablet:bottom-[100px] laptop:bottom-[100px] lg:bottom-[120px] left-1/2 -translate-x-1/2 w-full max-w-[1500px] h-auto tablet:max-h-[180px] laptop:max-h-[280px] lg:max-h-[350px] object-cover object-bottom z-0 opacity-30"
+        className=" tablet:block absolute bottom-[80px] tablet:bottom-[100px] laptop:bottom-[100px] lg:bottom-[143px] left-1/2 -translate-x-1/2 w-full max-w-[1500px] h-auto tablet:max-h-[180px] laptop:max-h-[280px] lg:max-h-[350px] object-cover object-bottom z-0 opacity-30"
       />
 
       {/* CONȚINUT */}
       <div className="relative z-10 flex flex-col tablet:grid tablet:grid-cols-2 max-w-[1440px] mx-auto px-4 tablet:px-6 py-16 tablet:py-20 gap-10 tablet:gap-12 laptop:gap-10 items-center">
         {/* TEXT – primul pe mobil */}
         <div className="order-1  tablet:order-2 tablet:-mt-12  tabletMd:-mt-12 laptop:mt-6 lg:mt-12 text-gray-800 max-w-3xl text-base tablet:text-[1.1rem] laptop:text-[1rem] lg:text-[1.5rem] leading-relaxed mb-10 laptop:mb-6 lg:mb-[500px]">
-          <h1 className="text-3xl phone:text-center tabletSm:text-7xl tabletSm:text-center tablet:text-5xl laptop:text-7xl lg:text-8xl font-fjalla text-[#b06b4c] leading-tight max-w-3xl pb-4">
+          <h1 className="text-3xl text-center tabletSm:text-7xl tabletSm:text-center tablet:text-5xl laptop:text-7xl lg:text-8xl  font-fjalla text-[#b06b4c] leading-tight max-w-3xl pb-4">
             Sprijin profesionist pentru{" "}
             <span className="text-amber-900">mame</span> dedicate
           </h1>
@@ -32,16 +32,12 @@ function Hero() {
             încredere prin fiecare etapă a alăptării.
           </p>
 
-<div className="flex flex-row flex-wrap justify-center gap-4 tabletSm:gap-6 lg:flex-row">
-  <PrimaryButton href="#booking">
-    Book a Free Consultation
-  </PrimaryButton>
-  <OutlineButton href="#services">
-    Explore my Services
-  </OutlineButton>
-</div>
-
-
+          <div className="flex flex-row flex-wrap justify-center gap-4 tabletSm:gap-6 lg:flex-row">
+            <PrimaryButton href="#booking">
+              Book a Free Consultation
+            </PrimaryButton>
+            <OutlineButton href="#services">Explore my Services</OutlineButton>
+          </div>
         </div>
 
         {/* IMAGINE – după pe mobil */}
